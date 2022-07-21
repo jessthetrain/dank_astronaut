35a7ccb946be2196e4869591d48ddd58c5a11720
Stones8000 <stagecoach7@outlook.com>
2022-07-21 21:40:45 +0100

    hi

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def on_member_join(member):
     embed = nextcord.Embed(title=f"Welcome to Dank Moon, {member}!",description=f"To get started in the server, read the rules in <#710840207808659517> and verify in <#741336727188144148>",color=nextcord.Color.magenta())
     embed.add_field(name="Once you have verified...",value="+ Get pings and other roles from <#711952053433401375>!\n+ Select your colour role in <#710847274988732507>!\n+ Chat with us in <#710573789309698060>!\n+ Our heists take place in <#711435197807067156>!\n+ See what we are giving away in <#711435312332537889> and <#809422611452919818>!")
     embed.set_footer("If you are here for a heist, don't freeload! We ban freeloaders :P")
-    embed.set_thumbnail(bot.user.avatar)
+    embed.set_thumbnail(bot.user.avatar.url)
     await member.send(embed=embed)
 
 bot.run("OTk5NzYwNDMwMDUyNDE3NjM4.GOfJE9.SzY__65AkGeN6rWRaTp4egYhl3gdWN6pm5my1g")
