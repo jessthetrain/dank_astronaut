0ae6035e151c5ceb840e25d46e5a8eaf5b863182
Stones8000 <stagecoach7@outlook.com>
2022-07-26 17:32:04 +0100

    hi

--- main.py
+++ main.py
@@ -159,11 +159,56 @@ async def donate(interaction:Interaction, duration:str=SlashOption(
         description=f"<:pink_arrow_right:1001505500296396890> **Time** <:purple_arrow_right:1001506139109863576> {duration}\n<:pink_arrow_right:1001505500296396890> **Winners** <:purple_arrow_right:1001506139109863576> {winners}\n<:pink_arrow_right:1001505500296396890> **Requirements** <:purple_arrow_right:1001506139109863576> {requirements}\n<:pink_arrow_right:1001505500296396890> **Prize** <:purple_arrow_right:1001506139109863576> {prize}\n<:pink_arrow_right:1001505500296396890> **Message** <:purple_arrow_right:1001506139109863576> {message}\n\n<:pink_arrow_right:1001505500296396890> Donated by {interaction.user.mention}",
         color=nextcord.Color.magenta()
     )
-    embed.set_image("https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
+    embed.set_thumbnail("https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
     embed.set_author(name=interaction.user,icon_url=interaction.user.display_avatar)
     donationChannel = bot.get_channel(741054074740539413)
     await interaction.response.send_message(content="✅ <#741054074740539413>",ephemeral=True)
     await donationChannel.send(content="<@&766651025364746260>",embed=embed)
+
+@giveaway.subcommand(name="ping", description="Ping the giveaway ping role (For staff)")
+async def gping(interaction:Interaction,donor:nextcord.Member=SlashOption(
+    name="donator",
+    description="Who donated to the giveaway?",
+    required=False
+), message:str=SlashOption(
+    name="message",
+    description="Message for the giveaway",
+    required=False
+)):
+    gman = interaction.guild.get_role(766651025364746260)
+    if gman in interaction.user.roles:
+        channel = interaction.channel
+        if donor==None and message==None:
+            channel.send(content="<@&711954068578500638>")
+        elif donor==None:
+            embed = nextcord.Embed(
+                title="🎉 Giveaway! 🎉",
+                description=f"<:pink_arrow_right:1001505500296396890> **Message:** {message}\n\n<:pink_arrow_right:1001505500296396890> Thank the donor in <#710573789309698060>",
+                color=nextcord.Color.magenta()
+            )
+            embed.set_thumbnail("https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
+            await interaction.response.send_message(content="✅",ephemeral=True)
+            channel.send(content="<@&711954068578500638>",embed=embed)
+        elif message==None:
+            embed = nextcord.Embed(
+                title="🎉 Giveaway! 🎉",
+                description=f"<:pink_arrow_right:1001505500296396890> **Donator:** {donor}\n\n<:pink_arrow_right:1001505500296396890> Thank the donor in <#710573789309698060>",
+                color=nextcord.Color.magenta()
+            )
+            embed.set_thumbnail("https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
+            await interaction.response.send_message(content="✅",ephemeral=True)
+            channel.send(content="<@&711954068578500638>",embed=embed)
+        else:
+            embed = nextcord.Embed(
+                title="🎉 Giveaway! 🎉",
+                description=f"<:pink_arrow_right:1001505500296396890> **Donator:** {donor}\n<:pink_arrow_right:1001505500296396890> **Message:** {message}\n\n<:pink_arrow_right:1001505500296396890> Thank the donor in <#710573789309698060>",
+                color=nextcord.Color.magenta()
+            )
+            embed.set_thumbnail("https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
+            await interaction.response.send_message(content="✅",ephemeral=True)
+            channel.send(content="<@&711954068578500638>",embed=embed)
+    else:
+        await interaction.response.send_message(content="❌ You are not a giveaway manager",ephemeral=True)
 
 
 bot.run("OTk5NzYwNDMwMDUyNDE3NjM4.GOfJE9.SzY__65AkGeN6rWRaTp4egYhl3gdWN6pm5my1g")
