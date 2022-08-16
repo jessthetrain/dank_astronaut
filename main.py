c5f1ca2705f26802b58febb265bb39ed40af0fc7
Stones8000 <stagecoach7@outlook.com>
2022-08-16 21:42:53 +0100

    hi

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ async def freeloaderperks(interaction:Interaction):
     embed = nextcord.Embed(title="🙅 Don't freeload! 🙅",description="Freeloading will result in your account being banned, meaning you miss out on all the future heists, giveaways, and other events we host on the server!",colour=nextcord.Color.magenta())
     embed.set_footer(text="Dank Moon",icon_url="https://cdn.discordapp.com/attachments/996446872451432498/999801982770491522/dank_moon.png")
     embed.set_thumbnail("https://cdn.discordapp.com/attachments/996446872451432498/1009198409741250630/1000681054119673856.gif")
-    await channel.send(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
 @freeloader.subcommand(name="catch")
 async def catchfreeloader(interaction:Interaction,freeloader:nextcord.User=SlashOption(name="freeloader",description="Who did you catch freeloading?",required=True)):
